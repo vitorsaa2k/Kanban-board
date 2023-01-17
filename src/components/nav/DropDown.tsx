43e57ee0764897138c1f51e9474b220360af9edb
vitoreow <vitoradriel66@gmail.com>
2023-01-16 21:48:42 -0300

    ui improvements

--- src/components/nav/DropDown.tsx
+++ src/components/nav/DropDown.tsx
@@ -9,14 +9,14 @@ export function DropDown({isShowing}: DropDownProps) {
 
   return (
     <div className={clsx(
-      "bg-my-gray-200 right-0 p-3 flex flex-col absolute transform transition-all duration-150 ease-in-out",
+      "bg-my-gray-200 w-28 rounded-lg p-2 right-0 flex flex-col absolute transform transition-all duration-150 ease-in-out",
       {
-        "opacity-0 translate-y-0 ": isShowing == false,
-        "opacity-100 translate-y-2 ": isShowing == true,
+        "opacity-0 translate-y-[-10px] ": isShowing == false,
+        "opacity-100 translate-y-0 ": isShowing == true,
       }
     )}>
-      <button><Text>Log Out</Text></button>
-      <button><Text>Delete</Text></button>
+      <button className="hover:bg-slate-400 rounded-lg px-2"><Text>Log out</Text></button>
+      <button className="hover:bg-slate-400 rounded-lg px-2"><Text>Delete</Text></button>
     </div>
   )
 }
