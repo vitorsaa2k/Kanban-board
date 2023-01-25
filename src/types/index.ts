ee214004376d23e6263ffa9fc32f792ba950b008
vitoreow <vitoradriel66@gmail.com>
2023-01-25 17:13:12 -0300

    Types file

---
+++ src/types/index.ts
@@ -0,0 +1,16 @@
+
+export interface UserType extends MarkerType {
+  name: string
+}
+
+export interface MarkerType {
+  title: string;
+  color: string;
+  tasks: TaskType[]
+}
+
+export interface TaskType {
+  task: string;
+  isCritical: boolean;
+  isDone: boolean;
+}
